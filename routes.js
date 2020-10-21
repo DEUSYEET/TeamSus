33cfda71de81753e8429b83b3f9f0637c09b7844
KingGopher <bgoldsberry@student.neumont.edu>
2020-10-21 10:11:52 -0600

    Updated Backend

--- routes.js
+++ routes.js
@@ -8,28 +8,46 @@ const Connect = (callBack) => {
     mongoose.connect("mongodb+srv://admin:admin@cluster0.un7p6.azure.mongodb.net/SusDataDB?retryWrites=true&w=majority", callBack);
 }
 
-routes.get("/GetData" = (req, res) => {
+routes.get("/css/style.css", (req, res) => {
+    res.sendFile(__dirname+'/public/css/style.css');
+});
+
+routes.get("/GetData", (req, res) => {
     Connect((err) => {
         
     })
 });
 
 routes.get("/Home", (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/html/index.html'));
+    res.sendFile(__dirname+'/public/views/index.html');
 });
 
 routes.get("/Results", (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/html/results.html'));
+    res.sendFile(__dirname+'/public/views/results.html');
 });
 
 routes.post("/ReceiveInput", (req, res) => {
     Connect((err) => {
-        
+        let newEntry = new User({
+            techPref: q1,
+            watchAnime: q2,
+            repairRights: q4,
+            muiscGenre: q3,
+            phonePref: q5,
+            countryVisit: q6,
+            employment: q7,
+            hobby: q8,
+            ruralOrUrban: q9,
+            dogsOrCats: q10,
+        });
+        newEntry.save((err) => {
+            console.log(err);
+        });
     })
 });
 
 routes.get("/Survey", (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/html/survey.html'));
+    res.sendFile(__dirname+'/public/views/survey.html');
 });
 
 module.exports = routes;
